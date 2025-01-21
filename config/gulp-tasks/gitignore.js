ce1c957a061c3d99fcf73dd07dd871d4abde1f61
DmitryUvarov <uvarov3123@gmail.com>
2025-01-21 18:25:24 +0200

    update test vercel

--- config/gulp-tasks/gitignore.js
+++ config/gulp-tasks/gitignore.js
@@ -3,7 +3,6 @@ export const gitignore = () => {
 	if (!fs.existsSync(".gitignore")) {
 		fs.writeFile("./.gitignore", "", cb)
 		fs.appendFile("./.gitignore", "phpmailer/\r\n", cb)
-		fs.appendFile("./.gitignore", "package-lock.json\r\n", cb)
 		fs.appendFile("./.gitignore", "flsStartTemplate/\r\n", cb)
 		fs.appendFile("./.gitignore", "node_modules/\r\n", cb)
 		fs.appendFile("./.gitignore", ".gitignore\r\n", cb)
